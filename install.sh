5cf0b9c344e2c9de81a4b8ea607246321f34f30b
root <cicatriz.r00t@gmail.com>
2017-05-09 18:03:53 +0000

    added installer and moved colors/ to vim/

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+dest="/root"
+for f in $( find . -maxdepth 1 \! -regex "\(.\|./\(.git\|README.md\|install.sh\)\)" -printf "%f\n" );do
+    if [ -e $dest/.$f ];then
+        echo "+ file exists, doing a backup of $f"
+        cp -rf $dest/.$f $dest/.$f.backup
+    fi
+    cp -rf ./$f $dest/.$f
+done
+
+if [ ! -e "$dest/.vim/bundle/Vundle.vim" ];then
+    git clone https://github.com/VundleVim/Vundle.vim.git $dest/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+fi
