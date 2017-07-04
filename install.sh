b8e7ebd8dc4f982e580edcb6c1542bfc0bd63369
cedriczirtacic <cicatriz.r00t@gmail.com>
2017-07-04 13:59:23 -0600

    added gef

--- install.sh
+++ install.sh
@@ -5,8 +5,17 @@ if [[ $(uname -s) == "Darwin" && $( which gfind) ]];then
     _find=gfind
 fi
 
+# clone and install gef
+if ! [ -d "./gef" ]; then
+    git clone https://github.com/hugsy/gef.git
+fi
+cat <<EOF > gdbinit
+source $(pwd)/gef/gef.py
+set disassembly-flavor att
+EOF
+
 [ ! -z $1 ] && dest=$1 || dest=$HOME
-for f in $( $_find . -maxdepth 1 \! -regex "\(.\|./\(.git\|README.md\|install.sh\)\)" -printf "%f\n" );do
+for f in $( $_find . -maxdepth 1 \! -regex "\(.\|./\(.git\|README.md\|install.sh\|gef\)\)" -printf "%f\n" );do
     if [ -e $dest/.$f ];then
         echo "+ file exists, doing a backup of $f"
         mv -f $dest/.$f $dest/.$f.backup
@@ -14,7 +23,7 @@ for f in $( $_find . -maxdepth 1 \! -regex "\(.\|./\(.git\|README.md\|install.sh
     ln -s $(pwd)/$f $dest/.$f
 done
 
-if [ ! -e "$dest/.vim/bundle/Vundle.vim" ];then
+if ! [ -e "$dest/.vim/bundle/Vundle.vim" ];then
     git clone https://github.com/VundleVim/Vundle.vim.git $dest/.vim/bundle/Vundle.vim
     vim +PluginInstall +PluginClean +qall
 fi
