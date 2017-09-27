cceadb612a5e8c346ca520ee5f47c4094f810180
cedriczirtacic <cicatriz.r00t@gmail.com>
2017-09-27 14:01:33 -0600

    various changes

--- install.sh
+++ install.sh
@@ -15,9 +15,11 @@ set disassembly-flavor att
 EOF
 
 [ ! -z $1 ] && dest=$1 || dest=$HOME
-for f in $( $_find . -maxdepth 1 \! -regex "\(.\|./\(.git\|README.md\|install.sh\|gef\)\)" -printf "%f\n" );do
+for f in $( $_find . -maxdepth 1 \! -regex "\(.\|./\(.git.*\|README.md\|install.sh\|gef\)\)" -printf "%f\n" );do
     if [ -e $dest/.$f ];then
         echo "+ file exists, doing a backup of $f"
+        # remove previous backups
+        [ -e $dest/.$f.backup ] && rm -f $dest/.$f.backup
         mv -f $dest/.$f $dest/.$f.backup
     fi
     ln -s $(pwd)/$f $dest/.$f
@@ -27,3 +29,6 @@ if ! [ -e "$dest/.vim/bundle/Vundle.vim" ];then
     git clone https://github.com/VundleVim/Vundle.vim.git $dest/.vim/bundle/Vundle.vim
     vim +PluginInstall +PluginClean +qall
 fi
+
+# load new bash source
+. $dest/.bashrc
